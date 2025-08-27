e39f42c4d68a88fedda17563beb7ae99cb02f904
Faza-Utomo <mnaufal250@gmail.com>
2025-08-27 17:38:22 +0700

    Sedikit perbaikan aowkoawk

--- supadmin/data/pemesanan/select.php
+++ supadmin/data/pemesanan/select.php
@@ -110,13 +110,13 @@ if (!isset($_SESSION["id_supadmin"])) {
   <!-- Content -->
   <div class="content-wrapper">
     <section class="content-header">
-      <h1>Data Persetujuan Media</h1>
+      <h1>Data Pengajuan Media</h1>
     </section>
 
     <section class="content">
       <div class="box">
         <div class="box-header with-border">
-          <h3 class="box-title">Data Persetujuan</h3>
+          <h3 class="box-title">Data yang menunggu untuk disetujui</h3>
         </div>
         <div class="box-body">
           <!-- contoh tabel -->
