a40e6acfd888bc9f1d5751975a6c9bb84f0abb2f
Faza-Utomo <mnaufal250@gmail.com>
2025-09-02 21:02:31 +0700

    update sedikit

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ if ( !isset($_SESSION["id_admin"])) {
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com" rel="preconnect">
   <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&family=Raleway:wght@300;400;500;600;700;900&family=Inter:wght@300;400;500;600;700;900&display=swap" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -38,13 +38,6 @@ if ( !isset($_SESSION["id_admin"])) {
   <!-- Main CSS File -->
   <link href="assets/css/main.css" rel="stylesheet">
 
-  <!-- =======================================================
-  * Template Name: Dewi
-  * Template URL: https://bootstrapmade.com/dewi-free-multi-purpose-html-template/
-  * Updated: Aug 07 2024 with Bootstrap v5.3.3
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
 </head>
 
 <body class="index-page">
@@ -53,8 +46,6 @@ if ( !isset($_SESSION["id_admin"])) {
     <div class="container-fluid container-xl position-relative d-flex align-items-center">
 
       <a href="index.php" class="logo d-flex align-items-center me-auto">
-        <!-- Uncomment the line below if you also wish to use an image logo -->
-        <!-- <img src="assets/img/logo.png" alt=""> -->
         <h1 class="sitename">HUMAS PROTOKOL</h1>
       </a>
 
@@ -79,7 +70,7 @@ if ( !isset($_SESSION["id_admin"])) {
 
       <div class="container d-flex flex-column align-items-center">
         <h2 data-aos="fade-up" data-aos-delay="100">SISTEM INFORMASI HUMAS PROTOKOL</h2>
-        <p data-aos="fade-up" data-aos-delay="200"><!-- isi something --></p>
+        <p data-aos="fade-up" data-aos-delay="200"></p>
         <div class="d-flex mt-4" data-aos="fade-up" data-aos-delay="300">
           <a href="#menu" class="btn-get-started">Mulai</a>
         </div>
@@ -90,18 +81,17 @@ if ( !isset($_SESSION["id_admin"])) {
     <!-- Services Section -->
     <section id="menu" class="services section">
 
-      <!-- Section Title -->
       <div class="container section-title" data-aos="fade-up">
         <h2>Menu</h2>
         <p>Menu Pengisian Media<br></p>
-      </div><!-- End Section Title -->
+      </div>
 
       <div class="container" data-aos="fade-up" data-aos-delay="100">
 
-        <div class="row gy-5">
-
+        <!-- baris pertama -->
+        <div class="row gy-5 justify-content-center">
           <div class="col-xl-4 col-md-6" data-aos="zoom-in" data-aos-delay="300">
-            <div class="service-item">
+            <div class="service-item h-100">
               <div class="img">
                 <img src="assets/img/services-2.jpg" class="img-fluid" alt="">
               </div>
@@ -112,13 +102,13 @@ if ( !isset($_SESSION["id_admin"])) {
                 <a href="form_media.php" class="stretched-link">
                   <h3>FORMULIR MEDIA</h3>
                 </a>
-                <p>Bagian untuk pengisian formulir media</p>
+                <p>Bagian untuk melakukan pengisian formulir pengajuan dari Media</p>
               </div>
             </div>
-          </div><!-- End Service Item -->
+          </div>
 
           <div class="col-xl-4 col-md-6" data-aos="zoom-in" data-aos-delay="300">
-            <div class="service-item">
+            <div class="service-item h-100">
               <div class="img">
                 <img src="assets/img/services-2.jpg" class="img-fluid" alt="">
               </div>
@@ -129,30 +119,16 @@ if ( !isset($_SESSION["id_admin"])) {
                 <a href="form_harian.php" class="stretched-link">
                   <h3>FORMULIR PENDATAAN HARIAN</h3>
                 </a>
-                <p>Info Link  </p>
+                <p>Bagian untuk pengisian data kiriman dari Media</p>
               </div>
             </div>
-          </div><!-- End Service Item -->
+          </div>
+        </div>
 
-          <div class="col-xl-4 col-md-6" data-aos="zoom-in" data-aos-delay="300">
-            <div class="service-item">
-              <div class="img">
-                <img src="assets/img/services-2.jpg" class="img-fluid" alt="">
-              </div>
-              <div class="details position-relative">
-                <div class="icon">
-                  <i class="bi bi-broadcast"></i>
-                </div>
-                <a href="form_bulanan.php" class="stretched-link">
-                  <h3>FORMULIR PENDATAAN BULANAN</h3>
-                </a>
-                <p>Info Link  </p>
-              </div>
-            </div>
-          </div><!-- End Service Item -->
-
+        <!-- baris kedua -->
+        <div class="row gy-5 mt-3 justify-content-center">
           <div class="col-xl-4 col-md-6" data-aos="zoom-in" data-aos-delay="400">
-            <div class="service-item">
+            <div class="service-item h-100">
               <div class="img">
                 <img src="assets/img/services-3.jpg" class="img-fluid" alt="">
               </div>
@@ -163,13 +139,13 @@ if ( !isset($_SESSION["id_admin"])) {
                 <a href="rekap.php" class="stretched-link">
                   <h3>REKAP PENGAJUAN</h3>
                 </a>
-                <p>Bagian untuk melihat rekap data dari pengajuan media</p>
+                <p>Bagian untuk melihat rekap data pengajuan yang telah dilakukan oleh Media</p>
               </div>
             </div>
-          </div><!-- End Service Item -->
+          </div>
 
           <div class="col-xl-4 col-md-6" data-aos="zoom-in" data-aos-delay="300">
-            <div class="service-item">
+            <div class="service-item h-100">
               <div class="img">
                 <img src="assets/img/services-2.jpg" class="img-fluid" alt="">
               </div>
@@ -180,13 +156,13 @@ if ( !isset($_SESSION["id_admin"])) {
                 <a href="rekap_harian.php" class="stretched-link">
                   <h3>REKAP HARIAN</h3>
                 </a>
-                <p>Info Link  </p>
+                <p>Bagian untuk melihat rekap data kiriman harian dari Media</p>
               </div>
             </div>
-          </div><!-- End Service Item -->
+          </div>
 
           <div class="col-xl-4 col-md-6" data-aos="zoom-in" data-aos-delay="300">
-            <div class="service-item">
+            <div class="service-item h-100">
               <div class="img">
                 <img src="assets/img/services-2.jpg" class="img-fluid" alt="">
               </div>
@@ -197,11 +173,10 @@ if ( !isset($_SESSION["id_admin"])) {
                 <a href="rekap_bulanan.php" class="stretched-link">
                   <h3>REKAP BULANAN</h3>
                 </a>
-                <p>Info Link  </p>
+                <p>Bagian untuk melihat rekap data kiriman bulanan dari Media</p>
               </div>
             </div>
-          </div><!-- End Service Item -->
-
+          </div>
         </div>
 
       </div>
@@ -210,60 +185,47 @@ if ( !isset($_SESSION["id_admin"])) {
 
     <!-- Contact Section -->
     <section id="contact" class="contact section">
-
-      <!-- Section Title -->
       <div class="container section-title" data-aos="fade-up">
         <h2>Contact</h2>
         <p></p>
-      </div><!-- End Section Title -->
-
+      </div>
       <div class="container" data-aos="fade-up" data-aos-delay="100">
-
         <div class="row gy-4">
           <div class="col-lg-6 ">
             <div class="row gy-4">
-
               <div class="col-lg-12">
                 <div class="info-item d-flex flex-column justify-content-center align-items-center" data-aos="fade-up" data-aos-delay="200">
                   <i class="bi bi-geo-alt"></i>
                   <h3>Address</h3>
                   <p>Jalan Raya Cijamil RT 03 RW 06, Kampung Cijamil, Ngamprah, Kec. Ngamprah</p>
                 </div>
-              </div><!-- End Info Item -->
-
+              </div>
               <div class="col-md-6">
                 <div class="info-item d-flex flex-column justify-content-center align-items-center" data-aos="fade-up" data-aos-delay="300">
                   <i class="bi bi-telephone"></i>
                   <h3>Call Us</h3>
                   <p>+62</p>
                 </div>
-              </div><!-- End Info Item -->
-
+              </div>
               <div class="col-md-6">
                 <div class="info-item d-flex flex-column justify-content-center align-items-center" data-aos="fade-up" data-aos-delay="400">
                   <i class="bi bi-envelope"></i>
                   <h3>Email Us</h3>
                   <p>info@dprd.com</p>
                 </div>
-              </div><!-- End Info Item -->
-
+              </div>
             </div>
           </div>
-
           <div class="col-lg-6">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3982.10686550714!2d107.51728356487622!3d-6.835396826866748!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e36dbb0cc061%3A0x9aec9b1ec9d25e6!2sDPRD%20kabupaten%20Bandung%20Barat!5e0!3m2!1sid!2sid!4v1755573333461!5m2!1sid!2sid" width="600" height="370" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-          </div><!-- End Contact Form -->
-
+          </div>
         </div>
-
       </div>
-
     </section><!-- /Contact Section -->
 
   </main>
 
   <footer id="footer" class="footer dark-background">
-
     <div class="container footer-top">
       <div class="row gy-4">
         <div class="col-lg-4 col-md-6 footer-about">
@@ -294,21 +256,15 @@ if ( !isset($_SESSION["id_admin"])) {
             <li><i class="bi bi-chevron-right"></i> <a href="#">Privacy policy</a></li>
           </ul>
         </div>
-
       </div>
     </div>
 
     <div class="container copyright text-center mt-4">
       <p>© <span>Copyright</span> <strong class="px-1 sitename">FajarDJ & Naufal Faza</strong> <span>All Rights Reserved</span></p>
       <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you've purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Pur  chase the pro version with working PHP/AJAX contact form: [buy-url] -->
         Designed by <a href="https://www.instagram.com/_fajardj/">FajarDJ</a> And <a href="https://www.instagram.com/naufal.paja/">Naufal Faza</a>
       </div>
     </div>
-
   </footer>
 
   <!-- Scroll Top -->
